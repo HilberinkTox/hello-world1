8e06e70c577a612778cf4ade666789bc10e03c78
Thomas <thomas.hilberink@gmail.com>
2018-12-12 16:31:18 +0100

    getter hinzugefügt

--- Helloworld.java
+++ Helloworld.java
@@ -1,4 +1,13 @@
-cd public class HelloWorld {
-   System.out.prinln("MOMO");
-   System.out.prinln("Default Dance"); 
+public class HelloWorld {
+
+    private int i;
+    public HelloWorld(){
+        i = 0; 
+    }
+
+    public int getI(){
+        return i;
+    }
+    System.out.prinln("MOMO");
+    System.out.prinln("Default Dance"); 
 }
